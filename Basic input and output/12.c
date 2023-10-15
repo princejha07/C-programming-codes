e76ef0dd09d181411efd357cf02a087bd6312774
Prince Jha <64372150+princejha07@users.noreply.github.com>
2023-10-15 09:27:02 +0530

    Add files via upload

---
+++ Basic input and output/12.c
@@ -0,0 +1,9 @@
+//Find output of below code: ineuron7
+#include<stdio.h>
+int main()
+{
+  int x;
+  x=printf("ineuron");
+  printf("%d",x);
+  return 0;
+}
---
+++ Basic input and output/5.c
@@ -0,0 +1,10 @@
+//WAP to calculate the length of String using printf function.
+#include<stdio.h>
+int main()
+{
+    int l;
+
+    l=printf("hello");
+    printf("length is %d",l);
+    return 0;
+}
