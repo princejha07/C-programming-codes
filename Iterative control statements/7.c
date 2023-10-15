1777181ab73cb5991e9379c8f30637f46657e1b0
Prince Jha <64372150+princejha07@users.noreply.github.com>
2023-10-15 09:38:20 +0530

    Add files via upload

---
+++ Iterative control statements/7.c
@@ -0,0 +1,9 @@
+// Write a program to print the first 10 even natural numbers in reverse order
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=10;i>0;i--)
+        printf("%d ",i*2);
+    return 0;
+}
