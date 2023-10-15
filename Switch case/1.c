15277a7f8ea2320d316eeda227c052e068c519d4
Prince Jha <64372150+princejha07@users.noreply.github.com>
2023-10-15 09:53:59 +0530

    Create 1.c

---
+++ Switch case/1.c
@@ -0,0 +1,51 @@
+// Write a program which takes the month number as an input and display number of days in that month.
+
+#include<stdio.h>
+int main()
+{
+    int m;
+    printf("enter number of month between 1 to 12.. ");
+    scanf("%d",&m);
+    switch(m)
+    {
+      case 1:
+          printf("January: 31 days");
+          break;
+      case 2:
+          printf("February: 28 or 29 days");
+          break;
+      case 3:
+          printf("March: 31 days");
+          break;
+      case 4:
+          printf("April: 30 days");
+          break;
+      case 5:
+          printf("May: 31 days");
+          break;
+      case 6:
+          printf("June: 30 days");
+          break;
+      case 7:
+          printf("July: 31 days");
+          break;
+      case 8:
+          printf("August: 31 days");
+          break;
+      case 9:
+          printf("September: 30 days");
+          break;
+      case 10:
+          printf("October: 31 days");
+          break;
+      case 11:
+          printf("November: 30 days");
+          break;
+      case 12:
+          printf("December: 31 days");
+          break;
+      default:
+         printf("Invalid month number");
+    }
+    return 0;
+}
